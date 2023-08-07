507bae7d9a3f96e28f0532b6785b4e33a7d4700a
Timo Makinen <tmakinen@foo.sh>
2023-08-07 14:20:09 +0000

    Fix syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -eu
 
-: "${PHP4DVD_DB_HOST:=localhost}"
-: "${PHP4DVD_DB_PORT:=3306}"
-: "${PHP4DVD_DB_NAME:=php4dvd}"
-: "${PHP4DVD_DB_USER:=php4dvd}"
-: "${PHP4DVD_DB_PASS:=}"
-: "${PHP4DVD_USER_GUESTVIEW:=false}"
+: "${PHP4DVD_DB_HOST:=${PHP4DVD_DB_HOST:-localhost}}"
+: "${PHP4DVD_DB_PORT:=${PHP4DVD_DB_PORT:-3306}}"
+: "${PHP4DVD_DB_NAME:=${PHP4DVD_DB_NAME:-php4dvd}}"
+: "${PHP4DVD_DB_USER:=${PHP4DVD_DB_USER:-php4dvd}}"
+: "${PHP4DVD_DB_PASS:=${PHP4DVD_DB_PASS:-}}"
+: "${PHP4DVD_USER_GUESTVIEW:=${PHP4DVD_USER_GUESTVIEW:-false}}"
 
 cat <<EOF > /var/www/html/config/config.php
 <?php
@@ -17,7 +17,7 @@ defined('DIRECTACCESS') OR exit('No direct script access allowed');
 \$settings["db"]["name"] = "${PHP4DVD_DB_NAME}";
 \$settings["db"]["user"] = "${PHP4DVD_DB_USER}";
 \$settings["db"]["pass"] = "${PHP4DVD_DB_PASS}";
-\$settings["user"]["guestview"] = ${PHP4DVD_USER_GUESTVIEW}";
+\$settings["user"]["guestview"] = ${PHP4DVD_USER_GUESTVIEW};
 EOF
 chown root:www-data /var/www/html/config/config.php
 chmod 640 /var/www/html/config/config.php
